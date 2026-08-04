727985bb943affef993d6b0a6b5f25d4fb485e3e
Aidan647 <kirillkotka@gmail.com>
2026-08-04 21:55:25 +0300

    Refactor file processing logic to streamline PNG and Lua handling

--- index.ts
+++ index.ts
@@ -27,11 +27,39 @@ const files = await fs.readdir(resolvedPath, { recursive: true })
 for (const file of files) {
 	const filePath = path.join(resolvedPath, file)
 	if (await fs.stat(filePath).then((stat) => !stat.isFile())) continue
-	if (!file.endsWith(".png")) continue
+	if (file.endsWith(".png")) 
+		await proccessPng(filePath)
+	if (file.endsWith(".lua"))
+		await proccessLua(filePath)
+	
+}
+
+async function proccessPng(file: string) {
+	if (!file.endsWith(".png")) return
 	console.log(`Processing ${file}...`)
-	await sharp(filePath)
+	await sharp(file)
 		.png({ compressionLevel: 9, force: true, palette: true, effort: 10 })
 		.toBuffer()
-		.then((buffer) => fs.writeFile(filePath, buffer))
+		.then((buffer) => fs.writeFile(file, buffer))
 		.catch((err) => console.error(`Error processing ${file}: ${err.message}`))
+}
+
+async function proccessLua(file: string) {
+	if (!file.endsWith(".lua")) return
+	const content = await fs.readFile(file, "utf-8")
+	const lines = content.split("\n")
+	const result: string[] = []
+	for (let i = 0; i < lines.length; i++) {
+		const line = lines[i]?.trim()
+		if (line === undefined) break
+		if (line === "") continue
+		if (line.startsWith("--")) continue
+		result.push(lines[i] ?? "")
+	}
+	const newContent = result.join("\n")
+	if (newContent !== content) {
+		await fs.writeFile(file, newContent, "utf-8")
+		console.log(`Processed ${file}`)
+	}
+
 }
